5e5f6585dc3467ed4310267b25707c6f42730835
shaistamomin2004-arch <shaistamomin2004@gmail.com>
2026-01-27 17:37:38 +0530

    Update model loading path in app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
 from tensorflow.keras.models import load_model
 
+st.set_page_config(page_title="Brain Tumor Detection", layout="wide")
+
+# 🔹 YOUR OLD CODE (slightly cleaned)
 @st.cache_resource
 def load_unet():
-    return load_model("/content/drive/MyDrive/unet_FINAL_GOOD.keras", compile=False)
+    return load_model("unet_FINAL_GOOD.keras", compile=False)
 
 model = load_unet()
+st.success("✅ Model loaded successfully!")
