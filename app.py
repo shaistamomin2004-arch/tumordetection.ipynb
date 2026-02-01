1c443491826ad5ddb8899a0fecbd50fe2ba72318
shaistamomin2004-arch <shaistamomin2004@gmail.com>
2026-02-01 19:47:14 +0530

    Refactor model loading in app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import gdown
 import streamlit as st
 import tensorflow as tf
 
-# 🔴 FORCE TensorFlow to use legacy Keras (THIS FIXES YOUR ERROR)
-os.environ["TF_USE_LEGACY_KERAS"] = "1"
-
 MODEL_URL = "https://drive.google.com/uc?id=1wDA7N46kMBAmu9m14ND-dsXawmO2gPrc"
 MODEL_PATH = "unet_FINAL_GOOD.keras"
 
@@ -13,7 +10,8 @@ MODEL_PATH = "unet_FINAL_GOOD.keras"
 def load_unet():
     if not os.path.exists(MODEL_PATH):
         gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
-    return tf.keras.models.load_model(MODEL_PATH, compile=False)
+    model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+    return model
 
 st.title("🧠 Brain Tumor Detection")
 
