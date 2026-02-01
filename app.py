fc4c6c09ae62be6be9ebac6e2308624935b1595c
shaistamomin2004-arch <shaistamomin2004@gmail.com>
2026-02-02 00:18:56 +0530

    Update model URL and path for loading U-Net

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ import gdown
 import streamlit as st
 import tensorflow as tf
 
-MODEL_URL = "https://drive.google.com/uc?id=1wDA7N46kMBAmu9m14ND-dsXawmO2gPrc"
-MODEL_PATH = "unet_FINAL_GOOD.keras"
+MODEL_URL = "https://drive.google.com/file/d/1WcJb68tqwR2gMsdy2hARO2J5y0mUCd6J/view?usp=drive_linkE"
+MODEL_PATH = "unet_tf219.keras"
 
 @st.cache_resource
 def load_unet():
     if not os.path.exists(MODEL_PATH):
         gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
-    model = tf.keras.models.load_model(MODEL_PATH, compile=False)
-    return model
+    return tf.keras.models.load_model(MODEL_PATH, compile=False)
 
 st.title("🧠 Brain Tumor Detection")
 
